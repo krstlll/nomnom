65b3b81f57bcf10fe55738d8e72670d9e482af9a
Kristel Angela Pastor <202101080@iacademy.edu.ph>
2024-07-28 01:48:26 +0800

    Place order page

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,69 +1,71 @@
-import React, { useContext } from 'react'
-import './Cart.css'
-import { StoreContext } from '../../context/StoreContext'
+import React, { useContext } from 'react';
+import './Cart.css';
+import { StoreContext } from '../../context/StoreContext';
+import { useNavigate } from 'react-router-dom';
 
 const Cart = () => {
-
-  const {cartItems,food_list,removeFromCart,getTotalCartAmount} = useContext(StoreContext);
+  const { cartItems, food_list, removeFromCart, getTotalCartAmount } = useContext(StoreContext);
+  const navigate = useNavigate();
 
   return (
     <div className='cart'>
       <div className='cart-items'>
         <div className='cart-items-title'>
-            <p>Items</p>
-            <p>Title</p>
-            <p>Price</p>
-            <p>Quantity</p>
-            <p>Total</p>
+          <p>Items</p>
+          <p>Title</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>Total</p>
         </div>
         <br />
         <hr />
-        {food_list.map((item,index)=>{
-            if(cartItems[item._id]>0){
-              return (
-                <div className='cart-items-title cart-items-item'>
-                  <img src={item.image} alt="" img/>
-                  <p>{item.name}</p>
-                  <p>Php {item.price}</p>
-                  <p>{cartItems[item._id]}</p>
-                  <p>Php {item.price * cartItems[item._id]}</p>
-                  <p onClick={()=>removeFromCart(item._id)} className='remove-btn'>Remove</p>
-                </div>
-              )
-            }
+        {food_list.map((item) => {
+          if (cartItems[item._id] > 0) {
+            return (
+              <div key={item._id} className='cart-items-title cart-items-item'>
+                <img src={item.image} alt={item.name} />
+                <p>{item.name}</p>
+                <p>$ {item.price}</p>
+                <p>{cartItems[item._id]}</p>
+                <p>$ {item.price * cartItems[item._id]}</p>
+                <p onClick={() => removeFromCart(item._id)} className='remove-btn'>Remove</p>
+              </div>
+            );
+          }
+          return null;
         })}
       </div>
       <div className='cart-bottom'>
-          <div className='cart-total'>
-              <h2>Cart Total</h2>
-              <div>
-                  <div className="cart-total-details">
-                    <p>Subtotal</p>
-                    <p>Php {getTotalCartAmount()}</p>
-                  </div>
-                  <hr />
-                  <div className="cart-total-details">
-                    <p>Delivery Fee</p>
-                    <p>Php {10}</p>
-                  </div>
-                  <hr />
-                  <div className="cart-total-details">
-                      <b>Total</b>
-                      <b>Php {getTotalCartAmount()+10}</b>
-                  </div>
-              </div>
-              <button>PROCCED TO CHECKOUT</button>
-          </div>
-          <div className='cart-promocode'>
-            <p>If available, enter your promo code here</p>
-            <div className='cart-promocode-input'>
-                <input type='text' placeholder ='promo code'/>
-                <button>Submit</button>
+        <div className='cart-total'>
+          <h2>Cart Total</h2>
+          <div>
+            <div className="cart-total-details">
+              <p>Subtotal</p>
+              <p>$ {getTotalCartAmount()}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Delivery Fee</p>
+              <p>$ {getTotalCartAmount()===0?0:3}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <b>Total</b>
+              <b>$ {getTotalCartAmount()===0?0:getTotalCartAmount() + 4}</b>
             </div>
           </div>
+          <button onClick={() => navigate('/placeOrder')}>PROCEED TO CHECKOUT</button>
+        </div>
+        <div className='cart-promocode'>
+          <p>If available, enter your promo code here</p>
+          <div className='cart-promocode-input'>
+            <input type='text' placeholder='promo code' />
+            <button>Submit</button>
+          </div>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
